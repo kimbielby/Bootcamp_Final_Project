61e4970eed9d5998e3ce388ce317c754009c2cc5
Kim Bielby <kb_f5@yahoo.com>
2016-05-07 19:12:58 +0200

    make redirects work and sign up populates the db

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,10 +7,26 @@ class UsersController < ApplicationController
   def new
   end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to user_path
+    else
+      redirect_to new_user_registration_path
+    end
+  end
+
   def sign_up
   end
 
   def show
     @user = User.find(params[:id])
+    render 'users/show'
+  end
+
+  private
+
+  def user_params
+     params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
   end
 end
